b64b31011fe15eba521d6a04808cb42e51cd58e4
melonin <hazagames404@gmail.com>
2021-08-17 17:01:05 +0700

    Update Nakama

--- source/nakama/Nakama.hx
+++ source/nakama/Nakama.hx
@@ -8,19 +8,26 @@ extern class Nakama {
     }
 
     @:luaDotMethod static function sync(func:Void->Void):Void;
+
     @:luaDotMethod static function create_client(config:ClientConfig):Client;
-    @:luaDotMethod static function create_api_account_device(uuid:String):Dynamic;
-    @:luaDotMethod static function create_api_account_email(email:String, password:String, ?vars:Dynamic):Dynamic;
     @:luaDotMethod static function get_account(client:Client, ?callback:Account->Void):Dynamic;
     @:luaDotMethod static function authenticate_device(client:Client, body_api_account_device:Dynamic, ?create:Bool, ?username:String, ?callback:Dynamic->Void):Dynamic;
     @:luaDotMethod static function authenticate_email(client:Client, body_api_account_email:Dynamic, ?create:Bool, ?username:String, ?callback:Dynamic->Void):Dynamic;
     @:luaDotMethod static function set_bearer_token(client:Client, bearer_token:Dynamic):Void;
+
+    @:luaDotMethod static function create_api_account_device(uuid:String):Dynamic;
+    @:luaDotMethod static function create_api_account_email(email:String, password:String, ?vars:Dynamic):Dynamic;
+
     @:luaDotMethod static function create_socket(client:Client):Socket;
     @:luaDotMethod static function socket_connect(socket:Socket):SocketConnectResult;
     @:luaDotMethod static function socket_send(socket:Socket, message:Dynamic):Dynamic;
+
     @:luaDotMethod static function on_matchpresence(socket:Dynamic, callback:MatchPresenceMessage->Void):Void;
     @:luaDotMethod static function on_matchdata(socket:Dynamic, callback:MatchMessage->Void):Void;
+
     @:luaDotMethod static function create_matchmaker_add_message(query:String, min_count:Int, max_count:Int, ?string_properties:Int, ?numeric_properties:Int):Dynamic;
+    @:luaDotMethod static function create_matchmaker_remove_message(ticket:MatchMakerTicket):Dynamic;
+    @:luaDotMethod static function create_match_data_message(match_id:String, op_code:Int, data:String):Dynamic;
 
 }
 
@@ -37,6 +44,7 @@ typedef ClientConfig = {
 
 extern class Client { }
 extern class Socket { }
+extern class MatchMakerTicket { }
 
 extern class Account {
 
