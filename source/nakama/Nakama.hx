1f8bd4ae6cfba78914bd934d480dcba180a94a43
melonin <hazagames404@gmail.com>
2021-11-25 19:29:04 +0700

    Add Nakama.update_account

--- source/nakama/Nakama.hx
+++ source/nakama/Nakama.hx
@@ -39,6 +39,8 @@ extern class Nakama {
     @:luaDotMethod static function create_match_join_message(?match_id:String, ?token:Dynamic):Dynamic;
     @:luaDotMethod static function create_match_data_message(match_id:String, op_code:Int, data:String):Dynamic;
 
+    @:luaDotMethod static function update_account(client:Client, body_api_update_account_request:Table<String, Dynamic>, ?callback:Dynamic->Void):Dynamic;
+
     @:luaDotMethod static function rpc_func(client:Client, func_id:String, ?body:Dynamic, ?http_key:String, ?callback:Dynamic->Void):{ payload:String };
     @:luaDotMethod static function rpc_func2(client:Client, func_id:String, ?payload:String, ?http_key:String, ?callback:Dynamic->Void):{ payload:String };
 
